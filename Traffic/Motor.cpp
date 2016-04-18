cd3cfc96be196e8f5670097ce4aabe4909d706c2
Christoph Wagner <christoph@christophs-macbook.local>
2016-04-18 10:16:35 +0200

    code Platzierung korrigiert

--- Traffic/Motor.cpp
+++ Traffic/Motor.cpp
@@ -7,19 +7,4 @@
 //
 
 #include "Motor.h"
-#include "Vehicle.h"
 
-class Motor : public Vehicle{
-public:
-    Motor();
-    ~Motor();
-    // füllt den Tank mit neuem Treibstoff auf, wenn der übergebene Treibstoff, die maximal aufnehmbare Menge übersteigt
-    // wird er abgelehnt und 'false' zurückgebeben
-    bool fillTank(double amount);
-    // gibt die aktuelle Menge des Treibstoffes zurück
-    double getFuelAmount();
-private:
-    double maxTank; // Maximales Fassungsvermögen des Treibstofftankes
-    double nowTank; // aktueller Füllstand des Treibstofftankes
-    double fuelPerLength;   // Kraftstoffverbrauch auf 100km
-};
--- Traffic/Motor.h
+++ Traffic/Motor.h
@@ -5,10 +5,24 @@
 //  Created by Christoph Wagner on 18/04/16.
 //  Copyright © 2016 de.christophwagner. All rights reserved.
 //
-
 #ifndef Motor_hpp
 #define Motor_hpp
 
-#include <stdio.h>
+#include "Vehicle.h"
+
+class Motor : public Vehicle{
+public:
+    Motor();
+    ~Motor();
+    // füllt den Tank mit neuem Treibstoff auf, wenn der übergebene Treibstoff, die maximal aufnehmbare Menge übersteigt
+    // wird er abgelehnt und 'false' zurückgebeben
+    bool fillTank(double amount);
+    // gibt die aktuelle Menge des Treibstoffes zurück
+    double getFuelAmount();
+private:
+    double maxTank; // Maximales Fassungsvermögen des Treibstofftankes
+    double nowTank; // aktueller Füllstand des Treibstofftankes
+    double fuelPerLength;   // Kraftstoffverbrauch auf 100km
+};
 
 #endif /* Motor_hpp */
