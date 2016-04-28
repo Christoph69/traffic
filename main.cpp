b3472be088d5f2c12541f1ee62c3b506e0f60dc5
Christoph Wagner <christoph@christophs-macbook.local>
2016-04-28 20:36:20 +0200

    neue Ordner Struktur

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  Traffic
+//
+//  Created by Christoph Wagner on 14/04/16.
+//  Copyright © 2016 de.christophwagner. All rights reserved.
+//
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "Motor.h"
+// #include "VehicleQueue.h"
+
+int main(int argc, const char * argv[]) {
+    //VehicleQueue liste;
+    
+    Vehicle *pV;
+    Motor Motor;
+    pV = &Motor;
+    // liste.append(pV);
+    pV->setSpeed(100, KILOMETER_PER_H);
+    std::this_thread::sleep_for (std::chrono::seconds(5));
+    std::cout << "Posi: " << pV->getPosition(METER) << std::endl;
+    // std::cout << "Fahrzeuge in der Liste: " << liste.size() << std::endl;
+    return 0;
+}
