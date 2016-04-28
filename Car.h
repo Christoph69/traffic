48557575fc6818d7ed1d4bf999777174e5494167
Christoph Wagner <christoph@christophs-macbook.local>
2016-04-28 22:09:06 +0200

    kleine Änderungen zur Anpassung an die VehicleQueue

--- Car.h
+++ Car.h
@@ -9,6 +9,11 @@
 #ifndef Car_h
 #define Car_h
 
+#include "MotorVehicle.h"
 
+class Car : public MotorVehicle {
+public:
+private:
+};
 
 #endif /* Car_h */
--- main.cpp
+++ main.cpp
@@ -9,19 +9,20 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include "MotorVehicle.h"
+#include "Car.h"
 #include "VehicleQueue.h"
 
 int main(int argc, const char * argv[]) {
     VehicleQueue liste;
     
-    Vehicle *pV;
-    MotorVehicle Motor;
-    pV = &Motor;
-    liste.append(pV);
-    pV->setSpeed(100, KILOMETER_PER_H);
+    //Vehicle *pV;
+    //MotorVehicle Motor;
+    //pV = &Motor;
+    //liste.append(pV);
+    //pV->setSpeed(100, KILOMETER_PER_H);
+    liste.createCar();
     std::this_thread::sleep_for (std::chrono::seconds(5));
-    std::cout << "Posi: " << pV->getPosition(METER) << std::endl;
+    //std::cout << "Posi: " << pV->getPosition(METER) << std::endl;
     std::cout << "Fahrzeuge in der Liste: " << liste.size() << std::endl;
     return 0;
 }
